698e775a2be0b86d17f6f092a3bea544ed6d4cdd
kasif780780 <33119279+kasif780780@users.noreply.github.com>
2018-12-24 01:35:53 +0530

    Adding Auto Complete with sql server database in

---
+++ site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+<script>
+    
+  </script>
+
+  $(document).ready(function(){
+    $("#searchInput").autocomplete({
+        source:function(request,response){
+          $.ajax({
+            url:'/City/Create1',
+            type:"POST",
+            dataType:"json",
+
+            data:{Prefix:request.term},
+            success :function(data){
+              response($.map(data,function(item){
+                return {label :item.Name,value:item.Name};
+              }));
+            },
+            messages:{
+                noResult:"",results:""
+            }
+          });
+        }
+      });
+  })
